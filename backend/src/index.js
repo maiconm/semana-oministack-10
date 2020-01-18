2d340429448b910c5a6c5fdfd3c90453e2ed2cbe
Maicon Andraski <maiconm23@gmail.com>
2020-01-18 20:09:39 -0300

    ❌ Added CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes); // this row needs to be after `app.use(express.json());`
 mongoose.connect('mongodb+srv://maicon:omnistack@cluster0-rgv9v.mongodb.net/week10?retryWrites=true&w=majority', {
