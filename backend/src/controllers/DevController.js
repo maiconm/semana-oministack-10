6f2aee8616010fc2f4d57e1cb335b0a880df5b68
Maicon Andraski <maiconm23@gmail.com>
2020-01-18 15:13:11 -0300

    📂 Folder structure
    - Created controller folter;
    - Moved functions to modules (DevController and TutorialController);

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(request, response) {
+        const { github_username, techs, lat, lon } = request.body;
+    
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+        const { name, login, avatar_url, bio } = apiResponse.data;
+    
+        const techsArray = techs.split(',').map(tech => tech.trim());
+    
+        const location = {
+            type: 'Point',
+            coordinates: [lon, lat]
+        };
+    
+        const dev = await Dev.create({
+            name: name || login,
+            github_username,
+            avatar_url,
+            bio: bio || 'Not defined',
+            techs: techsArray,
+            location
+        });
+    
+        return response.json(dev);
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,61 +1,19 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
+const TutorialController = require('./controllers/TutorialController');
 
 const routes = Router();
 
-routes.get('/', (_, response) => {
-    response.json({ hello: 'world!' });
-});
+routes.get('/', TutorialController.helloWorld);
 
-routes.get('/tutorial/users', (request, response) => {
-    console.log('GET - query params', request.query);
-    response.sendStatus(200);
-});
+routes.get('/tutorial/users', TutorialController.queryParamRequest);
 
-routes.put('/tutorial/users/:id', (request, response) => {
-    console.log('PUT - route params', request.params);
-    response.sendStatus(200);
-});
+routes.put('/tutorial/users/:id', TutorialController.putRouteParams);
 
-routes.delete('/tutorial/users/:id', (request, response) => {
-    console.log('DELETE - route params', request.params);
-    response.sendStatus(200);
-});
+routes.delete('/tutorial/users/:id', TutorialController.deleteRouteParams);
 
-routes.post('/tutorial/users/:name', (request, response) => {
-    console.log('Body', request.body);
-    response
-        .json(
-            {message: `${request.body.username} saved!`
-        })
-        .sendStatus(200);
-});
+routes.post('/tutorial/users/:name', TutorialController.bodyParams);
 
-routes.post('/devs', async (request, response) => {
-    const { github_username, techs, lat, lon } = request.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const { name, login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [lon, lat]
-    };
-
-    const dev = await Dev.create({
-        name: name || login,
-        github_username,
-        avatar_url,
-        bio: bio || 'Not defined',
-        techs: techsArray,
-        location
-    });
-
-    return response.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
