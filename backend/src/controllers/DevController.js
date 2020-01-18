bb224021cdf8af5a9d7a41f11054129b138b5df5
Maicon Andraski <maiconm23@gmail.com>
2020-01-18 15:45:03 -0300

    🔍 Check if user is registered

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,27 +4,32 @@ const Dev = require('../models/Dev');
 module.exports = {
     async store(request, response) {
         const { github_username, techs, lat, lon } = request.body;
-    
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        const { name, login, avatar_url, bio } = apiResponse.data;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
-        const location = {
-            type: 'Point',
-            coordinates: [lon, lat]
-        };
-    
-        const dev = await Dev.create({
-            name: name || login,
-            github_username,
-            avatar_url,
-            bio: bio || 'Not defined',
-            techs: techsArray,
-            location
-        });
-    
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+            const { name, login, avatar_url, bio } = apiResponse.data;
+
+            const techsArray = techs.split(',').map(tech => tech.trim());
+
+            const location = {
+                type: 'Point',
+                coordinates: [lon, lat]
+            };
+
+            dev = await Dev.create({
+                name: name || login,
+                github_username,
+                avatar_url,
+                bio: bio || 'Not defined',
+                techs: techsArray,
+                location
+            });
+        }
+
         return response.json(dev);
     }
 }
