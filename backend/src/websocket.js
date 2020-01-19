7cd232a342abd95a48f626a9efe6ce6aa4a2e38c
Maicon Andraski <maiconm23@gmail.com>
2020-01-19 13:20:28 -0300

    💾 Save connections

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,10 +1,21 @@
 const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+
+let connections = [];
 
 exports.setUpWebsocket = (server) => {
   const io = socketio(server);
 
   io.on('connection', socket => {
-    console.log(socket.id);
-    console.log(socket.handshake.query);
+    const { latitude, longitude, techs } = socket.handshake.query;
+
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+      },
+      techs: parseStringAsArray(techs),
+    })
   });
 };
