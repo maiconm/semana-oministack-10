e8f0bcd78367fc99367313e6c8f2f118a5d7aeca
Maicon Andraski <maiconm23@gmail.com>
2020-01-18 23:40:01 -0300

    🗡 Marker styling

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect } from 'react';
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -33,13 +33,47 @@ function Main() {
     return null;
   }
 
-  return (<MapView initialRegion={currentRegion} style={styles.map}/>);
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -25.5220547, longitude: -49.1939002 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/36269699?s=460&v=4' }}></Image>
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Maicon Andraski</Text>
+            <Text style={styles.devBio}>🐧🐧🐕🐕🐒🐒</Text>
+            <Text style={styles.devTechs}>Angular, Ionic, ReactJS, ReactNative, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
   },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+  callout: {
+    width: 260
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  devBio: {
+    color: '#666',
+    marginTop: 5
+  },
+  devTechs: {
+    marginTop: 5
+  }
 });
 
 export default  Main;
